e05e972227b3f38bc9bdb7cf20169eb263d5f08b
deivisjl <deivisjl@gmail.com>
2017-10-03 09:18:59 -0600

    Impl crud rol
    Implementacion de las operaciones para la entidad rol

---
+++ agregar-rol.php
@@ -0,0 +1,62 @@
+<?php 
+    include 'includes/redirect.php'; 
+    include 'includes/baseurl.php'; 
+    include 'views/header.php';
+    include 'views/sidebar.php';
+    include 'views/content.php';
+?>
+<!--=========================================================================================-->
+
+ <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Agregar Rol</h3>
+            </div>
+
+            <div class="box-body">
+              <div class="row">
+                  <div class="col-md-2">
+                      <a href="/rol.php"><span class="fa fa-arrow-circle-left fa-3x"></span></a>
+                  </div>
+              </div>  
+            </div>
+            
+        <div class="box-body">
+            <form role="form" method="post" action="/controllers/rol/rol.php">
+              <div class="box-body">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Nombre del rol</label>
+                        <input type="text" class="form-control" name="nombre" placeholder="Ingrese el nombre">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Descripcion</label>
+                        <input type="text" class="form-control" name="desc" placeholder="Ingrese una descripcion">
+                    </div>
+                 </div> 
+                
+              </div>
+              <!-- /.box-body -->
+
+              <input type="hidden" name="guardar">
+
+                <div class="box-footer">
+                  <button type="submit" class="btn btn-primary">Guardar</button>
+                </div>
+
+
+            </form>
+          
+        </div>
+            
+</div>
+
+
+
+
+<!--=========================================================================================-->
+
+<?php 
+
+	include 'views/footer.php'; 	
+?>
